fd4b6fed9d685b6dad150bb59117f4ff563c09ba
David_Arghand(cutyworm) <arghand.eng@gmail.com>
2018-07-20 17:19:58 +0430

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,5 +1,11 @@
 a=int(input('please enter the last number of your range ( : ? ) : '))
-b=int(input('please enter the first number of your range ( ? : ) : '))
+b=1
+while b < 2 :
+   b=int(input('please enter the first number of your range ( ? : ) : '))
+   if b<2:
+      print(f"The first number should be at least 2, Please select agian the first number")
+
+    
 prime_number = []
 for i in range(b,a+1):
     prime_number.append(i)
